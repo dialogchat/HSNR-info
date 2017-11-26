49225a4c88a88e1875e5281f48260950b033f149
dialogchat <dialogchat1@gmail.com>
2017-11-26 18:19:40 +0100

    Update honina.py

--- honina.py
+++ honina.py
@@ -15,8 +15,6 @@ import os, time
 import os.path
 import sys
 import json
-
-
 try:
     import apiai
 except ImportError:
@@ -25,62 +23,62 @@ except ImportError:
     )
     import apiai
 
-#insert your client token by api.ai:
+# insert your client token by api.ai:
 CLIENT_ACCESS_TOKEN = '68c5f3aafb344839b5de2c329dbb5ac9'
 
-#CLIENT_ACCESS_TOKEN:
-#client isa: 68c5f3aafb344839b5de2c329dbb5ac9
-#client honina: 27af35bde3364e6ea5c2efcd2eef097c
-#Developer access token:
+# CLIENT_ACCESS_TOKEN:
+# client isa: 68c5f3aafb344839b5de2c329dbb5ac9
+# client honina: 27af35bde3364e6ea5c2efcd2eef097c
+# Developer access token:
 
 def hibye(utterance):
-	#-------------------------------------------------------------------
-	#create welcome/good bye dialogues
-	#tts: gtts (mp3)
-	#-------------------------------------------------------------------
+	'''
+	create welcome/good bye dialogues
+	tts: gtts (mp3)
+	'''
     print ("Honina: "+ utterance)
-    tts = gTTS(text = utterance, lang='de')        #server tts query
-    tts.save(utterance[0:2]+".mp3")                #save file to disk
-    #os.startfile(utterance[0]+".mp3")             #win play file from disk
-    os.system("mpg321 -q "+utterance[0:2]+".mp3")  #linux play file from disk
-    os.remove(utterance[0:2]+".mp3")               #opt. mp3 removal
+    tts = gTTS(text = utterance, lang='de')        # server tts query
+    tts.save(utterance[0:2]+".mp3")                # save file to disk
+    #os.startfile(utterance[0]+".mp3")             # win play file from disk
+    os.system("mpg321 -q "+utterance[0:2]+".mp3")  # linux play file from disk
+    os.remove(utterance[0:2]+".mp3")               # opt. mp3 removal
 	
 
 def qaquery(user_input, ai):
-	#-------------------------------------------------------------------
-	#create qa query pairs
-	#nlp: api.ai (json)
-	#tts: gtts (mp3)
-	#-------------------------------------------------------------------
-    request = ai.text_request() #http request 
-    request.query = user_input  #json query
-    response = json.loads(request.getresponse().read().decode('utf8')) #json to dict
-    utterance = response['result']['fulfillment']['speech'] #answer
-    print ("Honina: "+utterance) 	          #print answer
-    tts = gTTS(text = utterance, lang='de')       #server tts query
-    tts.save(utterance[0:2]+".mp3")               #save file to disk
-    #os.startfile(utterance[0]+".mp3")            #win play file from disk
-    os.system("mpg321 -q "+utterance[0:2]+".mp3") #linux play file from disk
-    os.remove(utterance[0:2]+".mp3")              #opt. mp3 removal
+	'''
+	create qa query pairs
+	nlp: api.ai (json)
+	tts: gtts (mp3)
+	'''
+    request = ai.text_request() # http request 
+    request.query = user_input  # json query
+    response = json.loads(request.getresponse().read().decode('utf8')) # json to dict
+    utterance = response['result']['fulfillment']['speech'] # answer
+    print ("Honina: "+utterance) 	          # print answer
+    tts = gTTS(text = utterance, lang='de')       # server tts query
+    tts.save(utterance[0:2]+".mp3")               # save file to disk
+    #os.startfile(utterance[0]+".mp3")            # win play file from disk
+    os.system("mpg321 -q "+utterance[0:2]+".mp3") # linux play file from disk
+    os.remove(utterance[0:2]+".mp3")              # opt. mp3 removal
     
       
 def main():
     ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
     user_input=''  #init user_input
     
-    #welcome dialogues:
+    # welcome dialogues:
     hibye("Willkommen an der HSNR!!")
     hibye("Mein Name ist Honina.")
     hibye("Was moechtest du wissen?")
     
-    #main dialogue:
+    # main dialogue:
     while user_input != 'quit':
-        user_input = raw_input("You: ") #get user input (string)
-        #user_input = input("You: ") #get user input (string)
+        user_input = raw_input("You: ") # get user input (string)
+        # user_input = input("You: ") # get user input (string)
         if user_input != 'quit':
 			qaquery(user_input, ai)
 			
-	#good bye dialogue:
+	# good bye dialogue:
 	if user_input == 'quit':
 		hibye("Ok tschuess.")	
 
